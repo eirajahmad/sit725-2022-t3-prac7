2c808cf92933fb27b0c138867e5c113c907332a1
eirajahmad <46993669+eirajahmad@users.noreply.github.com>
2023-01-27 21:29:16 +1100

    changes socket
    changes

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
-
+//const bcrypt = require('bcrypt');
 //mongoDb connection...
 const MongoClient = require('mongodb').MongoClient;
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 //add database connections...
 const uri = 'mongodb+srv://Eiraj:password1234@cluster0.bu3yarb.mongodb.net/?retryWrites=true&w=majority' 
 const client = new MongoClient(uri, {useNewUrlParser: true})
 
 // insert project...
-const insertProjects = (project,callback) => {
-    projectCollection.insert(project,callback);
-}
 
 const getProjects = (callback) => {
     projectCollection.find({}).toArray(callback);
@@ -37,20 +36,7 @@ const createColllection = (collectionName) => {
     })
 }
 
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "images/kitten2.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
+
 
 app.get('/api/projects',(req,res) => {
     getProjects((err,result) => {
@@ -63,11 +49,11 @@ app.get('/api/projects',(req,res) => {
     })
 })
 
-// post api....
+// post sign up api....
 app.post('/api/projects',(req,res) => {
     console.log("New Project added", req.body)
     var newProject = req.body;
-    insertProjects(newProject,(err,result) => {
+    insertRegister(newProject,(err,result) => {
         if(err) {
             res.json({statusCode: 400, message: err})
         }
@@ -76,9 +62,52 @@ app.post('/api/projects',(req,res) => {
         }
     })
 })
+const insertRegister = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+app.post('/api/register', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+    
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+//login
+
 
 var port = process.env.port || 3000;
 app.listen(port,()=>{
     console.log("App listening to http://localhost:"+port)
     createColllection("Football")
 })
+
+//socket test
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    setInterval(()=>{
+      socket.emit('number', parseInt(Math.random()*10));
+    }, 1000);
+  
+  });
+  
