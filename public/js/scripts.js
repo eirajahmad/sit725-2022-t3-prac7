95bd3f18313765bf071cf3610546ab634d9b98bf
eirajahmad <46993669+eirajahmad@users.noreply.github.com>
2023-01-27 22:56:12 +1100

    socket
    socket

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -56,3 +56,14 @@ $(document).ready(function(){
     getProjects();
     $('.modal').modal();
   });
+
+  // connect to the socket
+
+let socket = io();
+
+
+socket.on('number', (msg) => {
+    console.log('Random number: ' + msg);
+})
+
+console.log('test')
--- server.js
+++ server.js
@@ -1,113 +1,32 @@
-var express = require("express")
-var app = express()
-var cors = require("cors")
-let projectCollection; 
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/public/index.html');
 
-app.use(express.static(__dirname+'/public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors())
-//const bcrypt = require('bcrypt');
-//mongoDb connection...
-const MongoClient = require('mongodb').MongoClient;
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
 
-//add database connections...
-const uri = 'mongodb+srv://Eiraj:password1234@cluster0.bu3yarb.mongodb.net/?retryWrites=true&w=majority' 
-const client = new MongoClient(uri, {useNewUrlParser: true})
-
-// insert project...
-
-const getProjects = (callback) => {
-    projectCollection.find({}).toArray(callback);
-}
-
-const createColllection = (collectionName) => {
-    client.connect((err,db) => {
-        projectCollection = client.db().collection(collectionName);
-        if(!err) {
-            console.log('MongoDB Connected')
-        }
-        else {
-            console.log("DB Error: ", err);
-            process.exit(1);
-        }
-    })
-}
-
-
-
-app.get('/api/projects',(req,res) => {
-    getProjects((err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
-        }
-        else {
-            res.json({statusCode: 200, message:"Success", data: result})
-        }
-    })
-})
-
-// post sign up api....
-app.post('/api/projects',(req,res) => {
-    console.log("New Project added", req.body)
-    var newProject = req.body;
-    insertRegister(newProject,(err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
-        }
-        else {
-            res.json({statusCode: 200, message:"Project Successfully added", data: result})
-        }
-    })
-})
-const insertRegister = (project,callback) => {
-    projectCollection.insert(project,callback);
-}
-
-app.post('/api/register', async (req, res) => {
-    try{
-        let foundUser = users.find((data) => req.body.email === data.email);
-        if (!foundUser) {
-    
-            let hashPassword = await bcrypt.hash(req.body.password, 10);
-    
-            let newUser = {
-                id: Date.now(),
-                username: req.body.username,
-                email: req.body.email,
-                password: hashPassword,
-            };
-            users.push(newUser);
-            console.log('User list', users);
-    
-            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
-        } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
-        }
-    } catch{
-        res.send("Internal server error");
-    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
 });
-//login
 
+// Socket.io server listens to our app
+const io = require('socket.io')(app);
 
-var port = process.env.port || 3000;
-app.listen(port,()=>{
-    console.log("App listening to http://localhost:"+port)
-    createColllection("Football")
-})
+// Send current time to all connected clients
+function sendTime() {
+    io.emit('time', { time: new Date().toJSON() });
+}
 
-//socket test
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    setInterval(()=>{
-      socket.emit('number', parseInt(Math.random()*10));
-    }, 1000);
-  
-  });
-  
+// Send current time every 10 secs
+setInterval(sendTime, 1000);
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('i am client', console.log);
+});
+
+app.listen(3000);
